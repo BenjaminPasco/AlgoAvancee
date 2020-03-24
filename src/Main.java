a1b3f19eaa4892c4a203701dcc91363018143597
Pasco <benjamin.pasco@hotmail.fr>
2020-03-24 16:59:45 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         int n = Integer.parseInt(/*args[0]*/ "20");
 
-        new problemeDynamique(n);
+        new problemeDynamique(n) ;
 
         //System.out.println(points.toString()+"\n");
     }
